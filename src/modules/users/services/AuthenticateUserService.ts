a903924a8c220bcfff1bb0e8bcbd0d44d12a5528
ygorthiago <ygorthiagoff@outlook.com>
2020-09-14 22:30:46 -0300

    Structure update (DDD)

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,11 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import User from '../models/user';
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
 
-import AppError from '../errors/AppError';
-
-import authConfig from '../config/auth';
+import authConfig from '@config/auth';
 
 interface Request {
   email: string;

ebbbbcc318f93b4e9482b22eb05a0659a3c83918
Ygor Thiago Farias <ygorthiagoff@outlook.com>
2020-12-20 16:58:48 -0300

    feat: fake notifications repository

---
+++ src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
@@ -0,0 +1,33 @@
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import Notification from '@modules/notifications/infra/typeorm/schemas/Notification';
+import { ObjectID } from 'mongodb';
+
+class NotificationsRepository implements INotificationsRepository {
+  private notifications: Notification[] = [];
+
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = new Notification();
+
+    Object.assign(notification, { id: new ObjectID(), content, recipient_id });
+
+    this.notifications.push(notification);
+
+    await this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationsRepository;

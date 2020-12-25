8e7723e730c89538682b891b1711814021cec855
Ygor Thiago Farias <ygorthiagoff@outlook.com>
2020-12-25 12:33:04 -0300

    chore: remove user mappers

--- src/modules/users/mappers/UserAuthMap.ts
+++
@@ -1,9 +0,0 @@
-import User from '../infra/typeorm/entities/User';
-
-export default class UserAuthMap {
-  public static toDTO(user: User): unknown {
-    return {
-      email: user.email,
-    };
-  }
-}

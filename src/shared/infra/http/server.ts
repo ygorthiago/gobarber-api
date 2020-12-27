0ac11308e7d0bef343770c1044c7de81eedc092c
Ygor Thiago Farias <ygorthiagoff@outlook.com>
2020-12-27 00:18:47 -0300

    feat: add rate limiter middleware

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/RateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -15,8 +16,8 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
-
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
